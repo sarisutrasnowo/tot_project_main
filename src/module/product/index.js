be7dddd4c006f6f938acb39907fe46098f3b3ca8
Gilang Pamungkas <fishwitheggplant@gmail.com>
2023-08-02 11:41:42 +0700

    second commit

--- src/module/product/index.js
+++ src/module/product/index.js
@@ -6,6 +6,7 @@ export default function ProductPage(){
       <Layout title='Product Page'>
         <main>
           <p>Product Page</p>
+          <h1>Product</h1>
         </main>
       </Layout>
     </>
